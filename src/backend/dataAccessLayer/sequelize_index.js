f2292b294af0dcdb19a0ee367131757f9847c30c
czagrzebski <48450104+czagrzebski@users.noreply.github.com>
2023-09-27 09:18:12 -0500

    Created Sequelize Instance

---
+++ src/backend/dataAccessLayer/sequelize_index.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const connection = require('../configs/connection.json');
+
+// Create a Sequelize instance
+const sequelize = new Sequelize(
+    connection.database,
+    connection.user,
+    connection.password,
+    {
+        host: connection.host,
+        dialect: 'mysql',
+        define: {
+            freezeTableName: true,
+            timestamps: true,
+            createdAt: 'CreatedDateTime', 
+            updatedAt: 'LastModifiedDateTime' 
+        },
+        logging: false
+    }
+)
+
+module.exports = sequelize;
