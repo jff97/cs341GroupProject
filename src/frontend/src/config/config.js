02925bda9f4934cede755c7ddd40ff2de5344df8
xiong8121 <144468442+xiong8121@users.noreply.github.com>
2023-12-06 14:20:48 -0600

    config
    config comments

--- src/frontend/src/config/config.js
+++ src/frontend/src/config/config.js
@@ -1,3 +1,6 @@
+//Author: Creed Zagrzebski
+//Date Created: Ocotber 7  2023 
+//Class & Methods Explained: This class is used to export information on the project
 const AppConfig = {
     appName: 'Bookify',
     appVersion: '1.0.0'
